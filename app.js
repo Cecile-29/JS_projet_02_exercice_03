4c380b00aa44127f1f34d22d1cdb1f9ac94f04a3
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-06 22:46:53 +0200

    push exercice_03

---
+++ app.js
@@ -0,0 +1,8 @@
+//**************Correction avec événement html dans l'input********
+//création d'une variable qui correspond à la chaîne de caractères du message
+//de la modale qui va s'afficher avec la méthode alert, et renvoyer sa value,
+// saisie ciblée avec la avec la méthode getElementById et fonction getInput
+function getInput(){
+  var inputAlert = document.getElementById('input').value;
+  alert(inputAlert);
+}
